882fdfa94817eff3ac4ddfd95161a5e2d50d8d67
lukmanpambudiii <lukmanpambudi.2020@student.uny.ac.id>
2024-07-05 16:07:51 +0700

    Update FLC_DriverL298N_V2.ino.ino
    FLC Menggunakan Driver L298N

--- FLC_DriverL298N_V2.ino.ino
+++ FLC_DriverL298N_V2.ino.ino
@@ -15,22 +15,34 @@ int motorSpeedB = 0;
 Fuzzy *fuzzy = new Fuzzy();
 
 // Mendefinisikan FuzzySet untuk input dan output
-
-// Input membership function untuk error
-
-FuzzySet *Kiri_Tajam = new FuzzySet(-70, -70, -50, -30);
-FuzzySet *Kiri = new FuzzySet(-40, -30, -20, -10);
-FuzzySet *Lurus = new FuzzySet(-10, -5, 5, 10);
-FuzzySet *Kanan = new FuzzySet(10, 20 , 30, 40);
-FuzzySet *Kanan_Tajam = new FuzzySet(30, 50, 70, 70);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//FuzzySet *Kiri_Tajam = new FuzzySet(-70, -70, -50, -30);
+//FuzzySet *Kiri = new FuzzySet(-40, -30, -20, -10);
+//FuzzySet *Lurus = new FuzzySet(-10, -5, 5, 10);
+//FuzzySet *Kanan = new FuzzySet(10, 20 , 30, 40);
+//FuzzySet *Kanan_Tajam = new FuzzySet(30, 50, 70, 70);
 
 // Input membership function untuk delta_error
-FuzzySet *Delta_Kiri_Tajam = new FuzzySet(-70, -50, -30, -30);
+//FuzzySet *Delta_Kiri_Tajam = new FuzzySet(-70, -50, -30, -30);
+//FuzzySet *Delta_Kiri = new FuzzySet(-40, -30, -20, -10);
+//FuzzySet *Delta_Lurus = new FuzzySet(-10, -5, 5, 10);
+//FuzzySet *Delta_Kanan = new FuzzySet(10, 20, 30, 40);
+//FuzzySet *Delta_Kanan_Tajam = new FuzzySet(30, 50, 70, 70);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+FuzzySet *Kiri_Tajam = new FuzzySet(-60, -50, -40, -30);
+FuzzySet *Kiri = new FuzzySet(-40, -30, -20, -10);
+FuzzySet *Lurus = new FuzzySet(-10, -5, 5, 10);
+FuzzySet *Kanan = new FuzzySet(10, 20, 30, 40);
+FuzzySet *Kanan_Tajam = new FuzzySet(30, 40, 50, 60);
+
+// Input membership function untuk delta_error
+FuzzySet *Delta_Kiri_Tajam = new FuzzySet(-60, -50, -40, -30);
 FuzzySet *Delta_Kiri = new FuzzySet(-40, -30, -20, -10);
 FuzzySet *Delta_Lurus = new FuzzySet(-10, -5, 5, 10);
 FuzzySet *Delta_Kanan = new FuzzySet(10, 20, 30, 40);
-FuzzySet *Delta_Kanan_Tajam = new FuzzySet(30, 50, 70, 70);
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+FuzzySet *Delta_Kanan_Tajam = new FuzzySet(30, 40, 50, 60);
+
 
 //// Output membership function (kecepatan motor kanan)
 //FuzzySet *RightStop = new FuzzySet(0, 0, 5, 10);
@@ -48,16 +60,16 @@ FuzzySet *Delta_Kanan_Tajam = new FuzzySet(30, 50, 70, 70);
 
 //// Output membership function (kecepatan motor kanan)
 //FuzzySet *RightStop = new FuzzySet(0, 0, 5, 10);
-FuzzySet *RightPelan = new FuzzySet(50, 50, 75, 100);
-FuzzySet *RightSedang = new FuzzySet(90, 115, 140, 165);
-FuzzySet *RightCepat = new FuzzySet(155, 180, 205, 205);
+FuzzySet *RightPelan = new FuzzySet(0, 0, 20, 40);
+FuzzySet *RightSedang = new FuzzySet(20, 40, 60, 80);
+FuzzySet *RightCepat = new FuzzySet(60, 80, 100, 120);
 
 
 //// Output membership function (kecepatan motor kiri)
 //FuzzySet *LeftStop = new FuzzySet(0, 0, 0.5, 1);
-FuzzySet *LeftPelan = new FuzzySet(50, 50, 75, 100);
-FuzzySet *LeftSedang = new FuzzySet(90, 115, 140, 165);
-FuzzySet *LeftCepat = new FuzzySet(155, 180, 205, 205);
+FuzzySet *LeftPelan = new FuzzySet(0, 0, 20, 40);
+FuzzySet *LeftSedang = new FuzzySet(20, 40, 60, 80);
+FuzzySet *LeftCepat = new FuzzySet(60, 80, 100, 120);
 
 float previousError = 0; // Menyimpan nilai error sebelumnya
 
@@ -113,7 +125,7 @@ void setup() {
     MotorKiri->addFuzzySet(LeftSedang);
     MotorKiri->addFuzzySet(LeftCepat);
     fuzzy->addFuzzyOutput(MotorKiri);
-
+    
     // Menambahkan aturan fuzzy menggunakan fungsi
     addFuzzyRule(1, Kiri_Tajam, Delta_Kiri_Tajam, RightCepat, LeftPelan); //Kiri_Tajam
     addFuzzyRule(2, Kanan_Tajam, Delta_Kanan_Tajam, RightPelan, LeftCepat); //Kanan_Tajam
@@ -123,7 +135,6 @@ void setup() {
     addFuzzyRule(5, Lurus, Delta_Lurus, RightSedang, LeftSedang);
     addFuzzyRule(6, Lurus, Delta_Kanan, RightSedang, LeftSedang);
     addFuzzyRule(7, Kanan, Delta_Kiri_Tajam, RightSedang, LeftSedang);
-
     //////////////// Kiri
     addFuzzyRule(8, Kiri_Tajam, Delta_Kiri, RightSedang, LeftPelan);
     addFuzzyRule(9, Kiri_Tajam, Delta_Lurus, RightSedang, LeftPelan); 
@@ -134,17 +145,16 @@ void setup() {
     addFuzzyRule(14, Kiri, Delta_Lurus, RightSedang, LeftPelan);
     addFuzzyRule(15, Kiri, Delta_Kanan_Tajam, RightSedang, LeftPelan);
     addFuzzyRule(16, Lurus, Delta_Kiri_Tajam, RightSedang, LeftPelan);
-
     //////////////// Kanan
-    addFuzzyRule(17, Lurus, Delta_Kanan_Tajam, RightSedang, LeftSedang);
-    addFuzzyRule(18, Kanan, Delta_Kiri, RightSedang, LeftSedang);
-    addFuzzyRule(19, Kanan, Delta_Lurus, RightSedang, LeftSedang);
-    addFuzzyRule(20, Kanan, Delta_Kanan, RightSedang, LeftSedang);
-    addFuzzyRule(21, Kanan, Delta_Kanan_Tajam, RightSedang, LeftSedang);
-    addFuzzyRule(22, Kanan_Tajam, Delta_Kiri_Tajam, RightSedang, LeftSedang);
-    addFuzzyRule(23, Kanan_Tajam, Delta_Kiri, RightSedang, LeftSedang);
-    addFuzzyRule(24, Kanan_Tajam, Delta_Lurus, RightSedang, LeftSedang);
-    addFuzzyRule(25, Kanan_Tajam, Delta_Kanan, RightSedang, LeftSedang);
+    addFuzzyRule(17, Lurus, Delta_Kanan_Tajam, RightPelan, LeftSedang);
+    addFuzzyRule(18, Kanan, Delta_Kiri, RightPelan, LeftSedang);
+    addFuzzyRule(19, Kanan, Delta_Lurus, RightPelan, LeftSedang);
+    addFuzzyRule(20, Kanan, Delta_Kanan, RightPelan, LeftSedang);
+    addFuzzyRule(21, Kanan, Delta_Kanan_Tajam, RightPelan, LeftSedang);
+    addFuzzyRule(22, Kanan_Tajam, Delta_Kiri_Tajam, RightPelan, LeftSedang);
+    addFuzzyRule(23, Kanan_Tajam, Delta_Kiri, RightPelan, LeftSedang);
+    addFuzzyRule(24, Kanan_Tajam, Delta_Lurus, RightPelan, LeftSedang);
+    addFuzzyRule(25, Kanan_Tajam, Delta_Kanan, RightPelan, LeftSedang);
 }
 
 void loop(){
@@ -158,11 +168,8 @@ void loop(){
 
       float deltaError = currentError - previousError; // Menghitung delta_error dari nilai previousError dikurangi error sebelumnya
 
-      Serial.print("error : ");
-      Serial.println(currentError);
-      Serial.print("delta error : ");
-      Serial.println(deltaError);
-
+      Serial.print("error : "); Serial.print(currentError ); Serial.print("delta error : "); Serial.println(deltaError );
+      
       fuzzy->setInput(1, currentError); // Mengatur input fuzzy untuk error
       fuzzy->setInput(2, deltaError); // Mengatur input fuzzy untuk delta_error
   
@@ -173,37 +180,42 @@ void loop(){
       float output2 = fuzzy->defuzzify(2);
 
 
-      Serial.print("Output fuzzy1: ");
-      Serial.println(output1);
-      Serial.print("Output fuzzy2: ");
-      Serial.println(output2);
-
+      Serial.print("Output fuzzy1: "); Serial.print(output1 ); Serial.print("Output fuzzy2: "); Serial.println(output2 );
+      
       // Mengatur kecepatan motor berdasarkan hasil defuzzifikasi dan aturan fuzzy
       int pwmValueKanan = 0;
       int pwmValueKiri = 0;
 
+//FuzzySet *LeftPelan = new FuzzySet(0, 0, 20, 40);
+//FuzzySet *LeftSedang = new FuzzySet(20, 40, 60, 80);
+//FuzzySet *LeftCepat = new FuzzySet(60, 80, 100, 120);
       
 //// Output membership function (kecepatan motor kiri)
 //FuzzySet *LeftStop = new FuzzySet(0, 0, 0.5, 1);
-//FuzzySet *LeftPelan = new FuzzySet(50, 50, 75, 100);
-//FuzzySet *LeftSedang = new FuzzySet(90, 115, 140, 165);
-//FuzzySet *LeftCepat = new FuzzySet(155, 180, 205, 205);
+//FuzzySet *LeftPelan = new FuzzySet(0, 0, 20, 40);
+//FuzzySet *LeftSedang = new FuzzySet(20, 40, 60, 80);
+//FuzzySet *LeftCepat = new FuzzySet(60, 80, 100, 120);
 
-       if (output1 >= 155 && output2 >= 50 && output2 <= 100) { // BelokKiriTajam
-        pwmValueKanan = 240; // Kecepatan motor kanan cepat
-        pwmValueKiri = 100; // Kecepatan motor kiri pelan
-      } else if (output1 >= 90 && output1 <= 165 && output2 >=50 && output2 <= 100) { // BelokKiri
-        pwmValueKanan = 200; // Kecepatan motor kanan sedang
-        pwmValueKiri = 100; // Kecepatan motor kiri pelan
-      } else if (output1 >= 90 && output1 <= 165 && output2 >= 90 && output2 <= 165) { // MajuLurus
-        pwmValueKanan = 200; // Kecepatan motor kanan sedang
-        pwmValueKiri = 200; // Kecepatan motor kiri sedang
-      } else if (output1 >= 50 && output1 <= 100 && output2 >= 90 && output2 <= 165) { // BelokKanan
-        pwmValueKanan = 100; // Kecepatan motor kanan rendah
-        pwmValueKiri = 200; // Kecepatan motor kiri sedang
-      } else if (output1 >= 50 && output1 <= 100 && output2 >= 155) { // BelokKananTajam
-        pwmValueKanan = 100; // Kecepatan motor kanan sedang
-        pwmValueKiri = 240; // Kecepatan motor kiri tinggi
+       if (output1 >= 60 && output2 >= 0 && output2 <= 40) { // BelokKiriTajam
+        Serial.println("Belok Kiri Tajam");
+        pwmValueKanan = 100; // Kecepatan motor kanan cepat
+        pwmValueKiri = 50; // Kecepatan motor kiri pelan
+      } else if (output1 >= 20 && output1 <= 80 && output2 >=0 && output2 <= 40) { // BelokKiri
+        Serial.println("Belok Kiri");
+        pwmValueKanan = 75; // Kecepatan motor kanan sedang
+        pwmValueKiri = 50; // Kecepatan motor kiri pelan
+      } else if (output1 >= 20 && output1 <= 80 && output2 >= 20 && output2 <= 80) { // MajuLurus
+        Serial.println("Maju Lurus");
+        pwmValueKanan = 50; // Kecepatan motor kanan sedang
+        pwmValueKiri = 50; // Kecepatan motor kiri sedang
+      } else if (output1 >= 0 && output1 <= 40 && output2 >= 20 && output2 <= 80) { // BelokKanan
+        Serial.println("Belok Kanan");
+        pwmValueKanan = 50; // Kecepatan motor kanan rendah
+        pwmValueKiri = 75; // Kecepatan motor kiri sedang
+      } else if (output1 >= 0 && output1 <= 40 && output2 >= 60) { // BelokKananTajam
+        Serial.println("Belok Kanan Tajam");
+        pwmValueKanan = 50; // Kecepatan motor kanan sedang
+        pwmValueKiri = 100; // Kecepatan motor kiri tinggi
       }
 
 
